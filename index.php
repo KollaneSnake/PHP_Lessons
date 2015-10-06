93efb5f3ad7a88ca8245f6ba81c44240f612c9a1
KollaneSnake <kollanemadu@gmail.com>
2015-10-06 19:25:19 +0300

    update

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Kiir php starting page</title>
+	<link rel="stylesheet" href="style.css">
+</head>
+<body>
+	<div id="wrapper">
+			<?php
+				echo "hello"
+			?>
+	</div>
+</body>
+</html>
